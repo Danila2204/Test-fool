31067474918f576004ebbb0c035aeec520c3c675
Данила Гаврютенков <pogranichnik.marshal@yandex.ru>
2022-06-25 23:24:58 +0300

    Версия теста на дурака 1.0 .

---
+++ app.js
@@ -0,0 +1,24 @@
+const body = document.body;
+
+const title = document.querySelector(".text");
+
+const container = document.querySelector(".choice");
+const [trueButton, falseButton] = document.querySelectorAll(".button");
+
+trueButton.addEventListener("click", () => {
+    title.textContent = "Так и знал)).";    
+})
+
+falseButton.addEventListener("mousemove", () => {
+    bodyStyle = getComputedStyle(document.body);
+
+    falseButton.style.position = "absolute";
+    falseButton.style.left = `${Math.round(Math.random() * parseFloat(bodyStyle.getPropertyValue("width")))}px`;
+    falseButton.style.top = `${Math.round(Math.random() * parseFloat(bodyStyle.getPropertyValue("height")))}px`;
+})
+
+falseButton.addEventListener("click", () => {
+    let audio = new Audio();
+    audio.src = "./no-please.mp3";
+    audio.autoplay = true;
+})
